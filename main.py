c59c1fb71a07d567de70ea729eedc21f96399d27
Katerina Solstad (katya sorok) <katya.solstad@gmail.com>
2020-09-27 01:05:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# The program "Time of Your Life)".
+# (task with mark ** is an optional challenge)
+
+'''0. Ask the user how long it takes to walk to school in minutes
+      (turn them to seconds) 
+      Ask the user the speed of walking in feet per second. 
+      Print to the user the estimate of distance to his or her school.
+      (Put distance to school in a variable and print that variable)
+      
+   1. Ask the user how many days a year he or she goes to school.
+      Print the time user spends on walking to school and back home
+      in a year.
+      
+   2. **Tell the user how much time he or she will save in a year
+      if he or she runs to school with a double speed of his or her walking.
+      (try to make your answer in hours/days)
+'''
+
+# help:
+'''
+   distance = time*speed
+   time=distance//speed  (you can try distance/time division and see the difference) 
+   1 minute = 60 seconds
+   1 hours = 60 minutes
+   1 day = 24 hours
+'''
